bbfbdad2da4184a272c57d0ea9f6ba05058fdac7
rouna <godemode12@gmail.com>
2023-04-19 13:47:53 +0530

    updated explicit type

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import {useState} from "react";
 import Link from "next/link";
 
-function CryptoDataFetcher(currency) {
+function CryptoDataFetcher(currency: string) {
     const {data, error} = useSWR(
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y&locale=en`,
         (url) =>
